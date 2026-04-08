90c5cd490485d282431dd1ef0b801aa016d77abe
edgar <ayalaalvarezedgarorlando@gmail.com>
2026-04-08 11:04:38 -0600

    subida de proyecto

---
+++ src/main/java/com/mycompany/sistemaeducativogestiondemeritos/SistemaEducativoGestionDemeritos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemaeducativogestiondemeritos;
+
+/**
+ *
+ * @author ayala
+ */
+public class SistemaEducativoGestionDemeritos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
