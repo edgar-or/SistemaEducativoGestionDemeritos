8b050fa9c49be47eb14403e88c9d4fad1564d07c
Irvin <estudiarlopez@gmail.com>
2026-04-10 13:06:53 -0600

    mmmm

--- src/main/java/com/mycompany/sistemaeducativogestiondemeritos/SistemaEducativoGestionDemeritos.java
+++ src/main/java/com/mycompany/sistemaeducativogestiondemeritos/SistemaEducativoGestionDemeritos.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.mycompany.sistemaeducativogestiondemeritos;
 
-/**
- *
- * @author ayala
- */
+
 public class SistemaEducativoGestionDemeritos {
 
     public static void main(String[] args) {
