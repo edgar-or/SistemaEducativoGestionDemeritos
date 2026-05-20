1825a987340f37585f9d46ce0ed76f0c5c738ddf
rene <pc25025@ues.edu.sv>
2026-05-20 14:28:58 -0600

    immmmmm

---
+++ src/main/java/DAO/MeritoDAO.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DAO;
+
+import DAO.conexion.Conexion;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import modelo.ModeloConducta;
+
+/**
+ *
+ * @author renec
+ */
+public class MeritoDAO {
+     public static List<ModeloConducta> obtenerTiposConducta() {
+        List<ModeloConducta> lista = new ArrayList<>();
+        String sql = "SELECT id_tipo_conducta, tipo, descripcion, puntos FROM tipo_conducta WHERE tipo= 'merito';";
+
+        try (Connection con = Conexion.getConexion(); 
+                 PreparedStatement ps = con.prepareStatement(sql); ResultSet rs = ps.executeQuery()) {
+
+            while (rs.next()) {
+                ModeloConducta conducta = new ModeloConducta(
+                        rs.getString("tipo"),
+                        rs.getString("descripcion"),
+                        rs.getInt("id_tipo_conducta"),
+                        rs.getInt("puntos")
+                );
+                lista.add(conducta);
+            }
+        } catch (Exception e) {
+            System.err.println("Error al obtener tipos de conducta: " + e.getMessage());
+        }
+        return lista;
+    }
+}
